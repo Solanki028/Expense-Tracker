fe7bd2efe3ba10011d4f1e1c8309adf972f72c60
unknown <you@example.com>
2024-12-03 00:04:40 +0530

    first commit

--- server.js
+++ server.js
@@ -29,9 +29,9 @@ app.use("/api/v1/users", require("./routes/userRoute")); // User routes
 app.use("/api/v1/transactions", require("./routes/transactionRoutes")); // Transactions routes
 
 // Serve static files in production
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, " 'client/build', 'index.html'"));
 });
 
 // Port configuration (either from .env or default to 8080)
